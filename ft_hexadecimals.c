85c3646586a928ff9f7d0536718f4f71a7d1bb46
SaraiMontesA <SaraiMontesA@gmail.com>
2024-02-14 18:30:19 +0100

    Subiendo todo

---
+++ ft_hexadecimals.c
@@ -0,0 +1,61 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hexadecimals.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarmonte <sarmonte@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 12:35:49 by sarmonte          #+#    #+#             */
+/*   Updated: 2024/02/14 12:58:10 by sarmonte         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "ft_printf.h"
+
+static int	ft_length_hexadecimal(unsigned int num);
+
+static void	ft_search_hexadecimal(unsigned int num, const char word);
+
+// Funtion for print a hexadecimal number
+int	ft_print_hexadecimal(unsigned int num, const char word)
+{
+	if (num == 0)
+		return (ft_print_character('0'));
+	else
+		ft_search_hexadecimal(num, word);
+	return (ft_length_hexadecimal(num));
+}
+// Function for search the hexadecimal number
+static void	ft_search_hexadecimal(unsigned int num, const char word)
+{
+	if (num >= 16)
+	{
+		ft_search_hexadecimal(num / 16, word);
+		ft_search_hexadecimal(num % 16, word);
+	}
+	else
+	{
+		if (num < 10)
+			ft_print_character(num + '0');
+		else
+		{
+			if (word == 'x')
+				ft_print_character(num - 10 + 'a');
+			if (word == 'X')
+				ft_print_character(num - 10 + 'A');
+		}
+	}
+}
+// Function for length of the hexadecimal number
+static int	ft_length_hexadecimal(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num / 16;
+	}
+	return (len);
+}
