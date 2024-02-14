6974a90e2ec3922db87e3d4dbd3384c81cda04c3
SaraiMontesA <SaraiMontesA@gmail.com>
2024-02-14 18:14:29 +0100

    Subiendo todo

---
+++ ft_basics.c
@@ -0,0 +1,70 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_basics.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarmonte <sarmonte@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 12:36:22 by sarmonte          #+#    #+#             */
+/*   Updated: 2024/02/14 12:57:38 by sarmonte         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "ft_printf.h"
+
+// Function for print a string
+int	ft_print_string(char *str)
+{
+	int	size;
+
+	size = 0;
+	if (str == 0)
+		return (ft_print_string("(null)"));
+	while (str[size])
+		size += ft_print_character(str[size]);
+	return (size);
+}
+
+// Function for print a number
+int	ft_print_number(int n)
+{
+	int	size;
+
+	size = 0;
+	if (n == 0)
+		size += ft_print_character('0');
+	if (n == -2147483648)
+	{
+		size += ft_print_string("-2147483648");
+		return (size);
+	}
+	if (n < 0)
+	{
+		size += ft_print_character('-');
+		n = -n;
+	}
+	if (n > 0)
+		size += ft_print_unsigned((unsigned int)n);
+	return (size);
+}
+// Function for print an unsigned number
+int	ft_print_unsigned(unsigned int n)
+{
+	int	size;
+
+	size = 0;
+	if (n == 0)
+		size += ft_print_character('0');
+	else
+	{
+		if (n / 10 != 0)
+			ft_print_unsigned(n / 10);
+		ft_print_character((n % 10) + '0');
+		while (n > 0)
+		{
+			n /= 10;
+			size++;
+		}
+	}
+	return (size);
+}
---
+++ ft_printf.h
@@ -0,0 +1,36 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarmonte <sarmonte@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 12:33:32 by sarmonte          #+#    #+#             */
+/*   Updated: 2024/02/14 12:43:05 by sarmonte         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+/* #########################################
+// #    INCLUDES OF EXTERNAL LIBRARIES     #
+// #######################################*/
+
+// It is used to handle variable number of function arguments.
+# include <stdarg.h>
+# include <unistd.h>
+
+/* #########################################	
+// #    LIST OF FUNCTIONS                  #
+// #######################################*/
+
+int	ft_print_character(int character);
+int	ft_printf(const char *str, ...);
+int	ft_print_string(char *str);
+int	ft_print_number(int n);
+int	ft_print_unsigned(unsigned int n);
+int	ft_print_hexadecimal(unsigned int num, const char word);
+int	ft_print_pointer(unsigned long long ptr);
+
+#endif
